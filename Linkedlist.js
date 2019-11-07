9f8b04bdb3e4abc066ee8d974f3cbe9b8d22481e
Steven Bull <stevenwbulljr@gmail.com>
2019-11-07 17:09:21 +0000

    finish the find and remove methods

--- Linkedlist.js
+++ Linkedlist.js
@@ -26,4 +26,41 @@ class LinkedList {
     }
   }
 
+  find(item) {
+    let currentNode = this.head;
+
+    if (!currentNode)
+      return null;
+
+    while (currentNode.value !== item) {
+      if (currentNode.next === null)
+        return null;
+      else
+        currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
+
+  remove(item) {
+    if (!this.head) {
+      return null;
+    }
+
+    if(this.head.value === item) {
+      this.head = this.head.next;
+      return;
+    }
+
+    let currentNode = this.head;
+    let previousNode = this.head;
+
+    while ((currentNode !== null) && (currentNode.value !== item)) {
+      previousNode = currentNode;
+      currentNode = currentNode.next;
+    }
+    if (currentNode === null) {
+      return 'Item not found';
+    }
+    previousNode.next = currentNode.next;
+  }
 }
